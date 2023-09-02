957b811429f548cc3971b9f196fb5b7cab110776
forggdev <141962334+forggdev@users.noreply.github.com>
2023-09-02 12:18:57 -0500

    First Modification
    Modified:
    Asignatura.java
    Grupo.java
    Persona.java
    Tipo.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -3,19 +3,17 @@ package classroom;
 public class Asignatura {
 
     public String nombre;
-    String n = nombre;
     public int codigoInterno;
     public int codigoExterno;
     public Tipo tipo;
 
     public Asignatura() {
-        this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
+        this("Sin nombre",0,0,Tipo.FUNDAMENTACION);;
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
+    //public Asignatura(int codigoInterno,Tipo tipo) {
+    //    this("Sin nombre", codigoInterno, 0,tipo);
+    //}
 
     public Asignatura(int codigoExterno,Tipo tipo) {
         this("Sin nombre", 0, codigoExterno,tipo);
@@ -39,9 +37,9 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
+    //public void cambiarDatos(int codigoInterno) {
+    //  this.codigoInterno = codigoInterno;
+    //}
 
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,7 +8,6 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -30,7 +29,14 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.cedula = 1;
+    	this.nombre = nombre;
+        totalPersonas++;
+    }
+    
+    public Persona() {
+        this.cedula = 0;
+    	this.nombre = "";
         totalPersonas++;
     }
     
